f63ac34f5f7d9bcdc23214a673c35a9eb4389bea
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-05-13 22:54:25 +0530

    Create JavaIterator.java
    An Iterator is an object that can be used to loop through collections, like ArrayList and HashSet. It is called an "iterator" because "iterating" is the technical term for looping. To use an Iterator, you must import it from the java.util package.

---
+++ Session_Programs/JavaIterator.java
@@ -0,0 +1,28 @@
+package ClassPrograms;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class JavaIterator {
+
+	public static void main(String[] args) {
+	
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+	    numbers.add(12);
+	    numbers.add(8);
+	    numbers.add(2);
+	    numbers.add(23);
+	    Iterator<Integer> it = numbers.iterator();
+	    while(it.hasNext()) {
+	      Integer i = it.next();
+	      if(i < 10) {
+	        it.remove();
+	      }
+	    }
+	    System.out.println(numbers);
+	}
+
+	/* OUTPUT
+	 * [12, 23]
+	 * */
+}
