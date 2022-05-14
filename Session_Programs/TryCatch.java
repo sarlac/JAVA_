8aac8d04bf4beb94145c37dcfa4ef4faf9bd669a
Sarla Ashokrao Chimegawe <sachimegawe@gmail.com>
2022-05-14 22:38:20 +0530

    Try Catch Exception program
    The try statement allows you to define a block of code to be tested for errors while it is being executed.
    The catch statement allows you to define a block of code to be executed, if an error occurs in the try block.
    The try and catch keywords come in pairs:

---
+++ Session_Programs/TryCatch.java
@@ -0,0 +1,40 @@
+package exceptionHandle;
+
+public class TryCatch {
+
+	public static void main(String[] args) {
+		try {
+			String s=  null;
+			System.out.println(s.length());
+		}
+		catch(Exception e) {
+			System.out.println(e.getMessage());
+			System.err.println(e.getMessage());
+		}
+		checkAge(16);
+		try {
+		      int[] myNumbers = {1, 2, 3};
+		      System.out.println(myNumbers[10]);
+		} 
+		catch (Exception e) {
+		      System.out.println("Something went wrong.");
+		      System.exit(0);  //this is only code which will not allow to execute finally
+		} 
+		finally {
+		      System.out.println("The 'try catch' is finished.");
+		}
+		
+	}
+		static void checkAge(int age) {
+	    try {
+			if (age < 18) {
+	      throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+	    }
+	    else {
+	      System.out.println("Access granted - You are old enough!");
+	    }
+	  }catch(ArithmeticException e) {
+		  System.out.println(e.getMessage());
+	  }
+	  }
+}
